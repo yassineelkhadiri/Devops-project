9465f3569db67d320be984ad969923c820b997ed
Yassine-Ayadi <82244304+Yassine-Ayadi@users.noreply.github.com>
2022-12-06 22:09:26 +0100

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -11,7 +11,7 @@ def buildImage() {
 def pushToNexus() {
     echo "pushing the jar file to Nexus maven-snapshots repo..."
     sh 'mvn clean install -Dmaven.test.skip=true'
-nexusArtifactUploader artifacts: [[artifactId: 'devops-project', classifier: '', file: 'target/devops-project-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'org.springframework.boot', nexusUrl: '20.13.123.42:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '0.0.1-SNAPSHOT'
+nexusArtifactUploader artifacts: [[artifactId: 'devops', classifier: '', file: 'target/devops-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'org.springframework.boot', nexusUrl: '20.13.123.42:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '0.0.1-SNAPSHOT'
 }
 
 def sonarScan() {
