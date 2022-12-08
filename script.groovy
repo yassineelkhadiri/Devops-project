9a63fcc5748a37def2a2fa9475c7fc31a08dfd4b
yassineelkhadiri <mohamedyassine.elkhadiri@insat.u-carthage.tn>
2022-12-08 21:21:08 +0100

    Correction : jenkins file

--- script.groovy
+++ script.groovy
@@ -1,6 +1,6 @@
 
 def buildImage() {
-    echo "building the docker image..."
+    echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'docker-hub-credentials', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
         sh 'docker build -t yassineelkhadiri/devops:spring-app-1.0 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
@@ -9,13 +9,13 @@ def buildImage() {
 }
 
 def pushToNexus() {
-    echo "pushing the jar file to Nexus maven-snapshots repo..."
+    echo 'pushing the jar file to Nexus maven-snapshots repo...'
     sh 'mvn clean install -Dmaven.test.skip=true'
-nexusArtifactUploader artifacts: [[artifactId: 'devops', classifier: '', file: 'target/devops-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'org.springframework.boot', nexusUrl: 'localhost:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '0.0.1-SNAPSHOT'
+    nexusArtifactUploader artifacts: [[artifactId: 'devops', classifier: '', file: 'target/devops-0.0.1-SNAPSHOT.jar', type: 'jar']], credentialsId: 'nexus-credentials', groupId: 'org.springframework.boot', nexusUrl: 'localhost:8081', nexusVersion: 'nexus3', protocol: 'http', repository: 'maven-snapshots', version: '0.0.1-SNAPSHOT'
 }
 
 def sonarScan() {
-    echo "Running sonarQube scan..."
+    echo 'Running sonarQube scan...'
     sh 'mvn test'
     sh 'mvn sonar:sonar -D sonar.login=sqa_609e3916d9f0e4ea56067e7c37f866c31c02f9dd'
 }
@@ -29,7 +29,7 @@ def sonarScan() {
 //        /* timeout(time: 10, unit: 'MINUTES') {
 //             waitForQualityGate abortPipeline: true
 //         } */
-        
+
 // }
 
 // def deployApp(String serverIp, String serverUser) {
